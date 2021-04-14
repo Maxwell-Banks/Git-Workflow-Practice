d1d08e5491bb1113340cbe4301f6099190fa92fb
Maxwell-Banks <maxwellmbanks@gmail.com>
2021-04-14 11:46:44 -0700

    Added Hellow World java file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,13 @@
-println("Hellow World!");
+/*class HelloWorld
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hellow World!");
+   }
+}*/
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
