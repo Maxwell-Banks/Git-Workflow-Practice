20f4e8d01f616d91bcdc908b325a91f2f2f52c7d
Maxwell-Banks <maxwellmbanks@gmail.com>
2021-04-14 12:19:59 -0700

    updated HelloWorld file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,3 @@
-/*class HelloWorld
-{
-   public static void main(String[] args)
-   {
-      System.out.println("Hellow World!");
-   }
-}*/
-
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
