9bcaaeeebd26835247ac326749611ca449f33304
Diego Garay <71237125+dcgaray@users.noreply.github.com>
2021-04-14 20:26:58 -0700

    Update HelloWorld.java
    sorry about this being so late!

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World, 3 years!");
+        //Diego here, these are my changes, in my hand is a man's worst nightmare......
+        System.out.println("a yo yo yo, yo can't stand right there!");
+        System.out.println("hahaha java go brrrrr!");
     }
 }
